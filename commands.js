663a4f01e6173f2d46eb70f1e55be26a3d70c762
StevenAprentisCodeur <41762297+StevenAprentisCodeur@users.noreply.github.com>
2018-07-27 00:29:53 +0200

    CommandHelp

---
+++ commands.js
@@ -0,0 +1,174 @@
+'use strict';
+
+const config = require('./config.json');
+const commandHelp = require('./help.js');
+const tool = require('./tool.js');
+const rp = require('request-promise');
+const stripInDent = require('strip-indent');
+const os = require('os');
+
+module.exports = {
+	'debug': debug,
+	'help': help,
+	'ban': ban,
+	'kick': kick
+}
+
+function debug(msg, bot){
+
+ let upTime = Math.round(os.uptime());
+ let upTime1 = Math.round(process.uptime());
+    console.log(upTime);
+     let upTimeSeconds2 = upTime1;
+        let upTimeOutput2 = "";
+        if (upTime<60) {
+            upTimeOutput2 = `${upTime1}s`;
+        } else if (upTime1<3600) {
+            upTimeOutput2 = `${Math.floor(upTime1/60)}m ${upTime1%60}s`;
+        } else if (upTime1<86400) {
+            upTimeOutput2 = `${Math.floor(upTime1/3600)}h ${Math.floor(upTime1%3600/60)}m ${upTime1%3600%60}s`;
+        } else if (upTime1<604800) {
+            upTimeOutput2 = `${Math.floor(upTime1/86400)}d ${Math.floor(upTime1%86400/3600)}h ${Math.floor(upTime1%86400%3600/60)}m ${upTime%86400%3600%60}s`;
+        }
+         let upTimeSeconds = upTime;
+        let upTimeOutput = "";
+
+        if (upTime<60) {
+            upTimeOutput = `${upTime}s`;
+        } else if (upTime<3600) {
+            upTimeOutput = `${Math.floor(upTime/60)}m ${upTime%60}s`;
+        } else if (upTime<86400) {
+            upTimeOutput = `${Math.floor(upTime/3600)}h ${Math.floor(upTime%3600/60)}m ${upTime%3600%60}s`;
+        } else if (upTime<604800) {
+            upTimeOutput = `${Math.floor(upTime/86400)}d ${Math.floor(upTime%86400/3600)}h ${Math.floor(upTime%86400%3600/60)}m ${upTime%86400%3600%60}s`;
+        }
+let embed_fields = [{
+                name: "System info:",
+                value: `${process.platform}-${process.arch} with ${process.release.name} version ${process.version.slice(1)}`,
+                inline: true
+            },
+            {
+                name: "Process info: PID",
+                value: `${process.pid}`,
+                inline: true
+            },
+            {
+                name: "Process memory usage:",
+                value: `${Math.ceil(process.memoryUsage().heapTotal / 1000000)} MB`,
+                inline: true
+            },
+            {
+                name: "System memory usage:",
+                value: `${Math.ceil((os.totalmem() - os.freemem()) / 1000000)} of ${Math.ceil(os.totalmem() / 1000000)} MB`,
+                inline: true
+            },
+            {
+                name: "Uptime bot:",
+                value: `:clock12: ${upTimeOutput}`,
+                inline: true
+            },
+            {
+                name: "Uptime computer:",
+                value: `:clock1230: ${upTimeOutput2}`,
+                inline: true
+            },{
+                name: 'Lib',
+                value: `**Discord.js**`
+            }
+        ];
+
+        msg.channel.send({
+            embed: {
+                author: {
+                    name: msg.author.username,
+                    icon_url: msg.author.avatarUrl,
+                    url:'http://google.fr'
+                },
+                color: 0x00FF00,
+                fields: embed_fields
+            }
+        });
+}
+
+function help(msg){
+	let args = msg.content.split(/\s+/).slice(1);
+
+	let helpStr;
+	if(args.length == 1){
+		if(args[0].charAt(0) == config.prefix) //['L', 'efjofefe']
+			args[0] = args[0].slice(1);
+		helpStr = commandHelp[args[0]];
+	    
+	}
+	if(helpStr)
+		msg.channel.send(helpStr, {
+			'code': 'css'
+		});
+	else 
+		msg.channel.send({embed: {
+    color: 3447003,
+    title: "Help",
+    fields: [{
+    		name: "Musique",
+			value: `${config.prefix}help music`,
+			inline: true
+      	},
+      	{ 
+       		name: "Ban",
+	   		value: `${config.prefix}help ban`,
+	   		inline: true
+      	},
+      	{
+        	name: "Kick",
+			value: `${config.prefix}help kick`,
+			inline: true
+      	},
+      	{
+			name: "debug",
+			value: `${config.prefix}debug`
+      	}
+    	],
+    	timestamp: new Date(),
+    footer: {
+      text: "©Steve & Co"
+    }
+  	}
+});
+}
+
+
+function ban(msg){
+	if ( !msg.member.hasPermission('BAN_MEMBERS')){
+		return msg.channel.send(`Vous n'avez pas la permission de ban`)
+	}
+	let memberToBan = msg.mentions.members.first();
+	if(memberToBan && memberToBan.bannable && (msg.member.highestRole.calculatedPosition >
+            memberToBan.highestRole.calculatedPosition || msg.guild.ownerID == msg.author.id)){
+		let reason = tool.parseOptionArg('raison', msg.content);
+	    let days = parseInt(tool.parseOptionArg('days', msg.content))
+
+	    let banOptions = {
+	    	days : days ? days : 0,
+	    	reason: reason ? reason: 'none'
+	    };
+	    memberToBan.ban(banOptions);
+	    msg.channel.send(` L\'utilisateur ${memberToBan} à bien été bani`)
+	}
+}
+
+function kick(msg){
+	if ( !msg.member.hasPermission('KICK_MEMBERS')){
+		return msg.channel.send(`Vous n'avez pas la permission de kick`)
+	}
+	let memberToKick = msg.mentions.members.first();
+	if(memberToKick && memberToKick.kickable && (msg.member.highestRole.calculatedPosition >
+            memberToKick.highestRole.calculatedPosition || msg.guild.ownerID == msg.author.id)){
+		let reason = tool.parseOptionArg('raison', msg.content);
+
+	    let kickOptions = {
+	    	reason: reason ? reason: 'none'
+	    };
+	    memberToKick.kick(kickOptions);
+	    msg.channel.send(` L\'utilisateur ${memberToKick} à bien été kick`)
+	}
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+const Discord = require("discord.js");
+const config = require("./config.json");
+const cmds = require("./commands.js");
+const music = require('./music.js')
+const tool = require('./tool.js');
+const prompt = require('prompt');
+const colors = require("colors");
+prompt.message = "";
+prompt.colors = '';
+
+const bot = new Discord.Client();
+
+bot.on('ready',() => {
+	console.log(`${bot.user.username} se lance`);
+	console.log(`le bot se lance sur ${bot.guilds.size} server`)
+
+	bot.user.setGame(config.prefix + 'help');
+});
+
+bot.on('message', msg => {
+	if(msg.author.bot || msg.channel.type != 'text')
+		return;
+
+	if(!msg.content.startsWith(config.prefix))
+		return;
+	let cmd =msg.content.split(/\s+/)[0].slice(config.prefix.length).toLowerCase();
+	getCmdFunction(cmd)(msg);
+})
+
+bot.on('error', (e) => console.log(e));
+bot.on('warn', (e) => console.log(e));
+//bot.on('debug', (e) => console.log(e));
+bot.login(config.token);
+
+function getCmdFunction(cmd){
+	const COMMANDS = {
+		'help': cmds.help,
+		'music': music.processCommands,
+		'debug': cmds.debug,
+		'ban': cmds.ban,
+		'kick': cmds.kick,
+
+	}
+	return COMMANDS[cmd] ? COMMANDS[cmd] : () => {};
+}
